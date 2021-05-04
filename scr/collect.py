e9d106bafd710f03fb0f6161a24d80c87470e820
Ondrej Biza <ondrej.biza@gmail.com>
2021-05-04 18:10:08 -0400

    collect data

--- scr/collect.py
+++ scr/collect.py
@@ -1,69 +1,193 @@
 import argparse
+import copy as cp
 import gym
 import numpy as np
+from PIL import Image
 import torch
 import torch.nn.functional as F
 from baby_a3c import NNPolicy, prepro
+import utils
+
+
+class RandomAgent(object):
+    """The world's simplest agent!"""
+
+    def __init__(self, action_space):
+
+        self.action_space = action_space
+
+    def act(self, observation, reward, done):
+
+        del observation, reward, done
+        return self.action_space.sample()
+
+
+def init_env(env_name, seed):
+
+    env = gym.make(env_name)
+    env.seed(seed)
+    num_actions = env.action_space.n  # get the action space of this game
+    return env, num_actions
+
+
+def init_and_load_model(hidden, num_actions, save_dir):
+
+    model = NNPolicy(channels=1, memsize=hidden, num_actions=num_actions)
+    step = model.try_load(save_dir)
+    assert step != 0
+    return model
+
+
+def init_random_agent(env):
+
+    return RandomAgent(env.action_space)
+
+
+def select_action(state, model, hx):
+
+    value, logit, hx = model((state.view(1, 1, 80, 80), hx))
+    logp = F.log_softmax(logit, dim=-1)
+    return torch.exp(logp).multinomial(num_samples=1).data[0]
+
+
+def reset_rnn_state():
+
+    return torch.zeros(1, 256)
+
+
+def preprocess_state(state):
+
+    return torch.tensor(prepro(state))
+
+
+def crop_normalize(img, crop_ratio):
+
+    img = cp.deepcopy(img)
+    img = img[crop_ratio[0]:crop_ratio[1]]
+    img = Image.fromarray(img).resize((50, 50), Image.ANTIALIAS)
+    return np.transpose(np.array(img), (2, 0, 1)) / 255
+
+
+def replay_init_episode(replay_buffer):
+
+    replay_buffer.append({
+        'obs': [],
+        'action': [],
+        'next_obs': [],
+        'state_ids': [],
+        'next_state_ids': []
+    })
 
 
 def main(args):
 
     hidden = 256
     max_episodes = args.max_episodes
+    dataset_save_path = args.save_path
     min_burnin = args.min_burnin
     max_burnin = args.max_burnin
     env_name = args.env_id
+    seed = args.seed
     save_dir = "./{:s}/".format(env_name.lower())
 
-    torch.manual_seed(0)
+    torch.manual_seed(seed)
 
-    env = gym.make(env_name)
-    env.seed(0)
-    num_actions = env.action_space.n  # get the action space of this game
+    env, num_actions = init_env(env_name, seed)
+    model = init_and_load_model(hidden, num_actions, save_dir)
+    random_agent = init_random_agent(env)
 
-    model = NNPolicy(channels=1, memsize=hidden, num_actions=num_actions)
-    step = model.try_load(save_dir)
-    assert step != 0
-
-    ep_steps = []
     episode_length, epr, eploss, done = 0, 0, 0, True
-    state = torch.tensor(prepro(env.reset()))
-    hx = torch.zeros(1, 256)
+    state = env.reset()
+    prev_state = state
+    hx = reset_rnn_state()
 
+    if env_name == 'PongDeterministic-v4':
+        crop = (35, 190)
+    elif env_name == 'SpaceInvadersDeterministic-v4':
+        crop = (30, 200)
+    else:
+        raise NotImplementedError("Only Pong and Space were used in the original paper.")
+
+    replay_buffer = []
+
+    # TODO: what are the max episodes in the envs, does the C-SWM repo change that?
     with torch.no_grad():
+
+        burnin_steps = np.random.randint(min_burnin, max_burnin)
+        replay_init_episode(replay_buffer)
 
         while True:
 
             episode_length += 1
-            value, logit, hx = model((state.view(1, 1, 80, 80), hx))
-            logp = F.log_softmax(logit, dim=-1)
-            action = torch.exp(logp).multinomial(num_samples=1).data[0]
 
-            state, reward, done, _ = env.step(action.numpy()[0])
+            start_collection = episode_length > burnin_steps
 
-            state = torch.tensor(prepro(state))
+            if start_collection:
+                replay_buffer[-1]['state_ids'].append(
+                    np.array(cp.deepcopy(env.unwrapped._get_ram()), dtype=np.int32)
+                )
+
+            if start_collection:
+                action = random_agent.act(None, None, None)
+            else:
+                action = select_action(preprocess_state(state), model, hx)
+                action = action.numpy()[0]
+
+            next_state, reward, done, _ = env.step(action)
+
+            if start_collection:
+                state_replay = np.concatenate(
+                    (crop_normalize(prev_state, crop), crop_normalize(state, crop)), axis=0
+                )
+                next_state_replay = np.concatenate(
+                    (crop_normalize(state, crop), crop_normalize(next_state, crop)), axis=0
+                )
+                replay_buffer[-1]['obs'].append(state_replay)
+                replay_buffer[-1]['next_obs'].append(next_state_replay)
+                replay_buffer[-1]['action'].append(action)
+                replay_buffer[-1]['next_state_ids'].append(
+                    np.array(cp.deepcopy(env.unwrapped._get_ram()), dtype=np.int32)
+                )
+
             epr += reward
             done = done or episode_length >= 1e4
 
-            if done:
-                hx = torch.zeros(1, 256)
-                print("episode done; steps: {:d}, return {:.2f}".format(episode_length, epr))
-                ep_steps.append(episode_length)
-                episode_length, epr, eploss = 0, 0, 0
-                state = torch.tensor(prepro(env.reset()))
+            prev_state = state
+            state = next_state
 
-                if len(ep_steps) >= max_episodes:
+            num_samples = len(replay_buffer[-1]['obs'])
+            if num_samples == 10:
+                done = True
+
+            if done:
+                print("ep {:d}".format(len(replay_buffer)))
+
+                hx = reset_rnn_state()
+                episode_length, epr, eploss = 0, 0, 0
+                state = env.reset()
+                prev_state = state
+
+                # check if episode was long enough
+                if num_samples != 10:
+                    del replay_buffer[-1]
+
+                # termination condition
+                if len(replay_buffer) == max_episodes:
                     break
 
-    print("episode steps, min: {:.1f}, max: {:.1f}, mean: {:.1f}".format(
-        np.min(ep_steps), np.max(ep_steps), np.mean(ep_steps)
-    ))
+                replay_init_episode(replay_buffer)
+
+    env.close()
+    utils.save_list_dict_h5py(replay_buffer, dataset_save_path)
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("env_id")
-parser.add_argument("min_burnin", type=int)
-parser.add_argument("max_burnin", type=int)
-parser.add_argument("max_episodes", type=int)
+parser.add_argument("--min-burnin", type=int, default=None)
+parser.add_argument("--max-burnin", type=int, default=None)
+parser.add_argument("--max-episodes", type=int, default=None)
+parser.add_argument("--num-steps", type=int, default=None)
+parser.add_argument("--save-path", default=None)
+parser.add_argument("--seed", type=int, default=0)
 parsed = parser.parse_args()
 main(parsed)
---
+++ sh/collect.sh
@@ -0,0 +1,5 @@
+python -m scr.collect PongDeterministic-v4 --min-burnin 50 --max-burnin 300 --max-episodes 1000 --num-steps 100 --save-path data/pong_full_train.h5 --seed 1
+python -m scr.collect PongDeterministic-v4 --min-burnin 50 --max-burnin 300 --max-episodes 10000 --num-steps 10 --save-path data/pong_full_eval.h5 --seed 2
+
+python -m scr.collect SpaceInvadersDeterministic-v4 --min-burnin 50 --max-burnin 300 --max-episodes 1000 --num-steps 100 --save-path data/spaceinvaders_full_train.h5 --seed 1
+python -m scr.collect SpaceInvadersDeterministic-v4 --min-burnin 50 --max-burnin 300 --max-episodes 10000 --num-steps 10 --save-path data/spaceinvaders_full_eval.h5 --seed 2
